431bb389fe72f9af6b53f0f6539f2d8100186033
teabitter <nataliadambaeva2@gmail.com>
2025-04-16 13:48:34 -0400

    Adding an Icon

--- Pomodoro/Form1.Designer.cs
+++ Pomodoro/Form1.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.lblTimer = new System.Windows.Forms.Label();
             this.btnStart = new System.Windows.Forms.Button();
             this.btnPause = new System.Windows.Forms.Button();
@@ -192,6 +193,7 @@
             this.Controls.Add(this.btnPause);
             this.Controls.Add(this.btnStart);
             this.Controls.Add(this.lblTimer);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.Text = "Pomodoro Timer";
             this.Load += new System.EventHandler(this.Form1_Load);
